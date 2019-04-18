444907ef7623054c34c2a9842b17f187cd0edc33
rsuttles58 <robert.suttles58@gmail.com>
2019-04-17 21:18:29 -0500

    Setup NPM dependencies and data table.

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+var bookBuds = [
+    {
+        "name": "dean",
+        "email": "dean@deanmail.com",
+        "scores": [5, 2, 3, 5, 3, 4, 3, 4, 5, 2],
+        "favorite_book": "The Count of Monte Cristo"
+    }
+]
