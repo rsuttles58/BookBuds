4f67687769614a307a22e54a9cbc0b9e049d9eb8
rsuttles58 <robert.suttles58@gmail.com>
2019-04-17 21:57:39 -0500

    Small refactoring.  Updating routes.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routing/apiRoutes")(app);
+// require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
